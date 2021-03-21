d415249a32a8d232bfd0a224b119451c3718c870
rahul kumar singh <rahulkumarsingh@rahuls-MacBook-Pro.local>
2021-03-21 14:56:38 +0530

    Future Builder Concept

--- lib/Screen/order_screen.dart
+++ lib/Screen/order_screen.dart
@@ -13,6 +13,72 @@ class OrderScreen extends StatefulWidget {
 }
 
 class _OrderScreenState extends State<OrderScreen> {
+
+  Future _ordersFuture;
+  Future _obtainOrdersFuture (){
+    return Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+  }
+
+  @override
+  void initState() {
+    _ordersFuture = _obtainOrdersFuture();
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('building orders');
+    //final orderData = Provider.of<Orders>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Orders'),
+      ),
+      drawer: AppDrawer(),
+      body: FutureBuilder(
+        future: _ordersFuture,
+        builder: (ctx, dataSnapShot) {
+          if (dataSnapShot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator(),);
+          }
+          else {
+            if (dataSnapShot.error != null) {
+              return Center(child: Text('An Error Occured'),);
+            }
+            else {
+              return Consumer<Orders>(builder: (ctx,orderData,child)=>ListView.builder(
+                itemCount: orderData.orders.length,
+                itemBuilder: (ctx, index) =>
+                    OrderItem(
+                      orderData.orders[index],
+                    ),),);
+            }
+          }
+        },),
+    );
+  }
+}
+
+
+
+/// IF there is an error in this code then we can revert back to this change
+//Here we are not future builder but normal stateful implementation
+
+/*import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../Providers/orders.dart' show Orders;
+import '../Widget/order_item.dart';
+import '../Widget/app_drawer.dart';
+
+class OrderScreen extends StatefulWidget {
+  static const routeName = '/order';
+
+  @override
+  _OrderScreenState createState() => _OrderScreenState();
+}
+
+class _OrderScreenState extends State<OrderScreen> {
   var _isLoading = false;
   @override
   void initState() {
@@ -20,15 +86,15 @@ class _OrderScreenState extends State<OrderScreen> {
       setState(() {
         _isLoading = true;
       });
-       await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-       setState(() {
-         _isLoading = false;
-       });
+      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+      setState(() {
+        _isLoading = false;
+      });
     });
     // TODO: implement initState
     super.initState();
   }
-  
+
   @override
   Widget build(BuildContext context) {
     final orderData = Provider.of<Orders>(context);
@@ -40,8 +106,9 @@ class _OrderScreenState extends State<OrderScreen> {
       body: _isLoading ? Center(child: CircularProgressIndicator(),):ListView.builder(
           itemCount: orderData.orders.length,
           itemBuilder: (ctx, index) => OrderItem(
-                orderData.orders[index],
-              )),
+            orderData.orders[index],
+          )),
     );
   }
 }
+*/
